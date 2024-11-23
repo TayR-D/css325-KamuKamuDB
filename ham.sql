095bb3e13b2e48a29d449dab65fafbb08e4dfca8
HamGNX <104154109+HamGNX@users.noreply.github.com>
2024-11-23 16:20:34 +0700

    doneall

--- ham.sql
+++ ham.sql
@@ -1,18 +1,13 @@
 -- Basic
---Top Seller
-SELECT Menu_Item_Name, Menu_Item_Size, SUM(Amount) AS Total_Quantity
-FROM Order_Item
-GROUP BY Menu_Item_Name, Menu_Item_Size
-ORDER BY Total_Quantity DESC
-LIMIT 5;
+--Menu Price > 70
+SELECT EN_Name, TH_Name, Menu_Group, Price
+FROM Menu_Item
+WHERE Price > 70;
 
---Revenue for each category
-SELECT mi.Menu_Category, SUM(oi.Amount * mi.Price) AS Total_Revenue
-FROM Order_Item oi
-JOIN Menu_Item mi ON oi.Menu_Item_Name = mi.EN_Name AND oi.Menu_Item_Size = mi.Size
-GROUP BY mi.Menu_Category
-ORDER BY Total_Revenue DESC;
-
+--Male Staff
+SELECT First_Name, Last_Name, Gender
+FROM Staff
+WHERE Gender = 'M';
 
 --Menu with nut allergy
 SELECT EN_Name, TH_Name, Menu_Group, Price
@@ -41,20 +36,6 @@ JOIN Includes i ON p.ID = i.Promo_ID
 GROUP BY p.ID, p._Name
 ORDER BY Usage_Count DESC;
 
---Ingredient Usage in Recipes (List all ingredients used in recipes along with the total quantity required for each.)
-SELECT 
-    b.Ingredient_Name,
-    i.Unit AS Measurement_Unit,
-    SUM(b.Amount * oi.Amount) AS Total_Amount_Used_Today
-FROM Order_Item oi
-JOIN Menu_Item mi ON oi.Menu_Item_Name = mi.EN_Name AND oi.Menu_Item_Size = mi.Size
-JOIN Brewing b ON b.Menu_Item_ID = mi.ID
-JOIN Ingredient i ON b.Ingredient_Name = i._Name
-JOIN _Order o ON oi.Order_NO = o.Order_NO
-WHERE DATE(o.Order_Timestamp) = CURDATE()  -- Replace CURDATE() with a specific date if needed
-GROUP BY b.Ingredient_Name, i.Unit
-ORDER BY Total_Amount_Used_Today DESC;
-
 --Revenue by Staff
 SELECT s.First_Name, s.Last_Name, SUM(oi.Amount * mi.Price) AS Total_Revenue
 FROM _Order o
@@ -62,4 +43,11 @@ JOIN Order_Item oi ON o.Order_NO = oi.Order_NO
 JOIN Menu_Item mi ON oi.Menu_Item_Name = mi.EN_Name AND oi.Menu_Item_Size = mi.Size
 JOIN Staff s ON o.Staff_ID = s.ID
 GROUP BY s.ID, s.First_Name, s.Last_Name
+ORDER BY Total_Revenue DESC;
+
+--Revenue by Menu Category
+SELECT mi.Menu_Category, SUM(oi.Amount * mi.Price) AS Total_Revenue
+FROM Order_Item oi
+JOIN Menu_Item mi ON oi.Menu_Item_Name = mi.EN_Name AND oi.Menu_Item_Size = mi.Size
+GROUP BY mi.Menu_Category
 ORDER BY Total_Revenue DESC;
